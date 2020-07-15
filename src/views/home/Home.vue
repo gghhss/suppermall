52586bf61b3d71ef5e814b6a8a1ae28274ee86b1
郭海生 <1358690951@qq.com>
2020-07-15 23:35:14 +0800

    添加首页轮播图及推荐

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="home">
-    <NavBar class="home-nav"><div slot="center">购物车</div></NavBar>
+    <NavBar class="home-nav"><div slot="center">购物街</div></NavBar>
+    <HomeSwiper :banners="banners"/>
+    <HomeRecommendView :recommends="recommends"/>
   </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import {getHomeMultidata} from "network/home"
+import NavBar from 'components/common/navbar/NavBar';
+import HomeSwiper from './childComps/HomeSwiper';
+import HomeRecommendView from './childComps/HomeRecommendView'
 
+import {getHomeMultidata} from "network/home";
 export default {
   name:"Home",
   components:{
-    NavBar
+    NavBar,
+    HomeSwiper,
+    HomeRecommendView
   },
   data(){
     return{
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,30 @@
+<template>
+  <Swiper>
+    <SwiperItem :key="item.title" v-for="item in banners">
+      <a href="item.link">
+        <img :src="item.image" alt />
+      </a>
+    </SwiperItem>
+  </Swiper>
+</template>
+
+<script>
+import {Swiper,SwiperItem} from 'components/common/swiper'
+export default {
+  name:'HomeSwiper',
+  props:{
+    banners:{
+      type:Array,
+      default(){
+        return []
+      }
+    }
+  },
+  components:{
+    Swiper,
+    SwiperItem
+  }
+};
+</script>
+<style scoped>
+</style>
